1f07688968716b1a19c363d6748de13a2e6840ee
Bruhsader <53186400+RJOOJ-JS@users.noreply.github.com>
2022-03-02 12:49:02 -0400

    Removing user from here

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { AnagramsController } from "./controllers/AnagramsController";
 import { AuthenticationController } from "./controllers/AuthenticationController";
-import { UsersController } from "./controllers/UsersController";
 import { WordsController } from "./controllers/WordsController";
 import { ensureAdmin } from "./middleware/ensureAdmin";
 import { ensureAuthentication } from "./middleware/ensureAuthentication";
@@ -11,7 +10,6 @@ const router = Router();
 // controllers
 const wordsController = new WordsController();
 const anagramsController = new AnagramsController();
-const usersController = new UsersController();
 const authenticationController = new AuthenticationController();
 
 
@@ -22,14 +20,10 @@ router.get("/", (req: Request, res: Response, next: NextFunction)=>{
 router.get("/words", wordsController.index);
 router.get("/words/:word", wordsController.show);
 
-router.post("/words", ensureAuthentication, ensureAdmin, wordsController.store);
-router.post("/users", usersController.store);
+router.post("/words", wordsController.store);
 
 router.put("/words/update", wordsController.update);
-router.delete("/words/remove/:word", ensureAuthentication, ensureAdmin, wordsController.remove);
+router.delete("/words/remove/:word", wordsController.remove);
 
 router.get("/anagrams/:word", anagramsController.index);
-router.get("/users", usersController.index);
-router.post("/users/auth", authenticationController.store);
-
 export default router;
